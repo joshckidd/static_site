f76989e90909c06e9a41627bef21578964c04e82
joshckidd <josh@larameekidd.com>
2025-08-23 16:03:33 -0400

    added some inline processing

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import *
 from htmlnode import *
+import re
 
 def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.TEXT:
@@ -14,6 +15,34 @@ def text_node_to_html_node(text_node):
         return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"alt": text_node.text, "src": text_node.url})
+    
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if delimiter not in node.text or node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            in_delimited_text = False
+            text_splits = node.text.split(delimiter)
+            for split in text_splits:
+                if in_delimited_text == False:
+                    if split != '':
+                        new_nodes.append(TextNode(split, TextType.TEXT))
+                    in_delimited_text = True
+                else:
+                    if split != '':
+                        new_nodes.append(TextNode(split, text_type))
+                    in_delimited_text = False
+            if in_delimited_text == False:
+                raise Exception("Invalid markdown.")
+    return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
 
 def main():
     tn = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
--- src/test_main.py
+++ src/test_main.py
@@ -43,5 +43,82 @@ class TestMain(unittest.TestCase):
         self.assertEqual(html_node.value, "")
         self.assertEqual(html_node.props, {"alt": "This is a text node", "src": "http://www.test.com/"})
 
+    def test_code_split(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+                                        TextNode("This is text with a ", TextType.TEXT),
+                                        TextNode("code block", TextType.CODE),
+                                        TextNode(" word", TextType.TEXT),
+                                    ])
+
+    def test_code_split_end(self):
+        node = TextNode("This is text with a `code block`", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+                                        TextNode("This is text with a ", TextType.TEXT),
+                                        TextNode("code block", TextType.CODE),
+                                    ])
+
+    def test_code_split_beginning(self):
+        node = TextNode("`code block` is code", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+                                        TextNode("code block", TextType.CODE),
+                                        TextNode(" is code", TextType.TEXT),
+                                    ])
+        
+    def test_code_split_two(self):
+        node = TextNode("This is `a code block` and a `code block` too", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+                                        TextNode("This is ", TextType.TEXT),
+                                        TextNode("a code block", TextType.CODE),
+                                        TextNode(" and a ", TextType.TEXT),
+                                        TextNode("code block", TextType.CODE),
+                                        TextNode(" too", TextType.TEXT),
+                                    ])
+
+    def test_bold_split(self):
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+                                        TextNode("This is text with a ", TextType.TEXT),
+                                        TextNode("bold", TextType.BOLD),
+                                        TextNode(" word", TextType.TEXT),
+                                    ])
+
+    def test_italic_split(self):
+        node = TextNode("This is text with a _italic_ word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+                                        TextNode("This is text with a ", TextType.TEXT),
+                                        TextNode("italic", TextType.ITALIC),
+                                        TextNode(" word", TextType.TEXT),
+                                    ])
+    def test_extract_markdown_images_one(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links_one(self):
+        matches = extract_markdown_links(
+            "This is text with an [link](https://i.imgur.com/)"
+        )
+        self.assertListEqual([("link", "https://i.imgur.com/")], matches)
+
+    def test_extract_markdown_images_multiple(self):
+        matches = extract_markdown_images(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        )
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_markdown_links_multiple(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
+
 if __name__ == "__main__":
     unittest.main()
