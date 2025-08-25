166b417d769a3288438684b39d678f036d933849
joshckidd <josh@larameekidd.com>
2025-08-25 12:05:04 -0400

    determine block types

--- src/split_blocks.py
+++ src/split_blocks.py
@@ -1,2 +1,43 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    QUOTE = "quote"
+    CODE = "code"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+def block_to_block_type(block):
+    if block[0:2] == "# " or block[0:3] == "## " or block[0:4] == "### " or block[0:5] == "#### " or block[0:6] == "##### " or block[0:7] == "###### ":
+        return BlockType.HEADING
+    if block[0:3] == "```" and block[-3:] == "```":
+        return BlockType.CODE
+    lines = block.split("\n")
+    line_start = ""
+    for line in lines:
+        if line_start == "":
+            if line[0] == ">":
+                line_start = ">"
+            elif line[0:2] == "- ":
+                line_start = "- "
+            elif line[0:3] == "1. ":
+                line_start = "1. "
+            else:
+                return BlockType.PARAGRAPH
+        elif len(line_start) == 1 and line[0] != line_start:
+            return BlockType.PARAGRAPH
+        elif len(line_start) == 2 and line[0:2] != line_start:
+            return BlockType.PARAGRAPH
+        elif len(line_start) == 3:
+            if str(int(line_start[0]) + 1) + ". " != line[0:3]:
+                return BlockType.PARAGRAPH
+            line_start = line[0:3]
+    if line_start == ">":
+        return BlockType.QUOTE
+    elif line_start == "- ":
+        return BlockType.UNORDERED_LIST
+    return BlockType.ORDERED_LIST
+
 def markdown_to_blocks(markdown):
     return list(map(lambda x: x.strip(), markdown.split("\n\n")))
--- src/test_split_blocks.py
+++ src/test_split_blocks.py
@@ -21,3 +21,69 @@ This is the same paragraph on a new line
                     "- This is a list\n- with items",
                 ],
             )
+
+        def test_block_to_blocktype(self):
+            md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+# This is a header
+
+## This is also a header
+
+### This too is a header
+
+#### Still a header
+
+##### Still a header!
+
+###### Still a header!
+
+####### PAragraph!
+
+#Also paragraph
+
+>This is quote
+>By Joshua Kidd
+
+>This is not a quote
+Because not all lines start with >
+
+```
+This is a code block.
+```
+
+1. This is a list
+2. With numbered items
+
+- This is a list
+- with items
+
+-This is not a list
+- There needs to be a space
+
+2. This is also not a list
+3. The numbers aren't in order
+4. It's a paragraph instead
+"""
+            blocks = markdown_to_blocks(md)
+            self.assertEqual(block_to_block_type(blocks[0]), BlockType.PARAGRAPH)
+            self.assertEqual(block_to_block_type(blocks[1]), BlockType.PARAGRAPH)
+            self.assertEqual(block_to_block_type(blocks[2]), BlockType.HEADING)
+            self.assertEqual(block_to_block_type(blocks[3]), BlockType.HEADING)
+            self.assertEqual(block_to_block_type(blocks[4]), BlockType.HEADING)
+            self.assertEqual(block_to_block_type(blocks[5]), BlockType.HEADING)
+            self.assertEqual(block_to_block_type(blocks[6]), BlockType.HEADING)
+            self.assertEqual(block_to_block_type(blocks[7]), BlockType.HEADING)
+            self.assertEqual(block_to_block_type(blocks[8]), BlockType.PARAGRAPH)
+            self.assertEqual(block_to_block_type(blocks[9]), BlockType.PARAGRAPH)
+            self.assertEqual(block_to_block_type(blocks[10]), BlockType.QUOTE)
+            self.assertEqual(block_to_block_type(blocks[11]), BlockType.PARAGRAPH)
+            self.assertEqual(block_to_block_type(blocks[12]), BlockType.CODE)
+            self.assertEqual(block_to_block_type(blocks[13]), BlockType.ORDERED_LIST)
+            self.assertEqual(block_to_block_type(blocks[14]), BlockType.UNORDERED_LIST)
+            self.assertEqual(block_to_block_type(blocks[15]), BlockType.PARAGRAPH)
+            self.assertEqual(block_to_block_type(blocks[16]), BlockType.PARAGRAPH)
+             
