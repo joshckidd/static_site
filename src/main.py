5ad95e05ea1e1b8832928afa537def437ac378d4
joshckidd <josh@larameekidd.com>
2025-08-26 08:56:14 -0400

    copy site files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 from textnode import *
+import os, shutil
+
+def copy_file(path, files):
+    for file in files:
+        if os.path.isfile(os.path.join("static/", path, file)):
+            shutil.copy(os.path.join("static/", path, file), os.path.join("public/", path, file))
+        else:
+            os.mkdir(os.path.join("public/", path, file))
+            copy_file(os.path.join(path, file), os.listdir(os.path.join("static/", path, file)))
+
+def generate_site():
+    if os.path.exists("public/"):
+        shutil.rmtree("public/")
+    os.mkdir("public/")
+    copy_file("", os.listdir("static/"))
+    
 
 def main():
-    tn = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(tn)
+    generate_site()
 
 main()
