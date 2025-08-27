cfce4c72f8a53d06173a1c54a5d53d62252249a1
joshckidd <josh@larameekidd.com>
2025-08-27 09:16:32 -0400

    generate full site

--- src/main.py
+++ src/main.py
@@ -30,9 +30,16 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as file:
         template = file.write(html)
     
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    files = os.listdir(dir_path_content)
+    for file in files:
+        if os.path.isfile(os.path.join(dir_path_content, file)):
+            generate_page(os.path.join(dir_path_content, file), template_path, os.path.join(dest_dir_path, file.split(".")[0] + ".html"))
+        else:
+            generate_pages_recursive(os.path.join(dir_path_content, file), template_path, os.path.join(dest_dir_path, file))
 
 def main():
     generate_site()
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content/", "template.html", "public/")
 
 main()
